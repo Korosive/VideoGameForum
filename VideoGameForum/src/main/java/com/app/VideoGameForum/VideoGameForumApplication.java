d218c41bf482f2c484dadbcfe55d7d988b78b365
Korosive <eddoman33433@gmail.com>
2020-12-05 15:26:55 +1100

    Base files

---
+++ VideoGameForum/src/main/java/com/app/VideoGameForum/VideoGameForumApplication.java
@@ -0,0 +1,13 @@
+package com.app.VideoGameForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameForumApplication.class, args);
+	}
+
+}
---
+++ VideoGameForum/src/test/java/com/app/VideoGameForum/VideoGameForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.VideoGameForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
