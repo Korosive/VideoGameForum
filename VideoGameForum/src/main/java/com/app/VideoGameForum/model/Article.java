2bb1054597724494680cb79ce00f5e3f1f07359e
Korosive <eddoman33433@gmail.com>
2020-12-05 15:45:31 +1100

    Created article model in Java

---
+++ VideoGameForum/src/main/java/com/app/VideoGameForum/model/Article.java
@@ -0,0 +1,53 @@
+package com.app.VideoGameForum.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID article_id;
+
+    @Column(name = "user_id")
+    private UUID user_id;
+
+    @Column(name = "paragraph_id")
+    private UUID paragraph_id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "date_created")
+    private Date date_created;
+
+    public Article(UUID article_id, UUID user_id, UUID paragraph_id, String title, Date date_created) {
+        this.article_id = article_id;
+        this.user_id = user_id;
+        this.paragraph_id = paragraph_id;
+        this.title = title;
+        this.date_created = date_created;
+    }
+
+    public UUID getArticle_id() {
+        return article_id;
+    }
+
+    public UUID getUser_id() {
+        return user_id;
+    }
+
+    public UUID getParagraph_id() {
+        return paragraph_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+}
